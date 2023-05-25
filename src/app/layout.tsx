e1e7ef56d33b3f59c839b8c174a8c0228e836dbd
mc_fdc <mc-fdc@tuna2134.jp>
2023-05-26 07:45:44 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        {children}
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
+        <div className="flex-grow">
+          <Header />
+          {children}
+        </div>
+        <footer>hi</footer>
       </body>
     </html>
   )
