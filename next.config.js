cdf0f41b9d80d24828b75926174dbc40ba3681d3
mc_fdc <Masato.04.11.2007@gmail.com>
2023-05-28 23:48:18 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-if (process.env.BUILD_TYPE === "edge") {
-  nextConfig.experimental = { runtime: 'edge'}
+if (process.env.BUILD_TYPE === "standalone") {
+  nextConfig.output = "standalone";
 }
 
 module.exports = nextConfig
