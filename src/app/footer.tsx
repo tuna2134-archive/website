b5fad6ed0bd33c0b4e3d572e935baa4d469308e8
mc_fdc <Masato.04.11.2007@gmail.com>
2023-05-29 11:32:50 +0900

    Update footer.tsx

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="h-64 w-full max-w-4xl mx-auto items-center border-t">
-      <div className="flex">
-        <h2 className="text-3xl">tuna2134</h2>
+    <footer className="h-64 border-t">
+      <div className="w-full max-w-4xl mx-auto items-center">
+        <div className="flex">
+          <h2 className="text-3xl">tuna2134</h2>
+        </div>
       </div>
     </footer>
   )
