e0d41f1c6c70420eb4a6b3e7e687bff475dfa440
mc_fdc <Masato.04.11.2007@gmail.com>
2023-05-30 14:16:03 +0900

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,11 @@
 import { getBlogs } from "./_blogs";
 import BlogComponent from "../_components/blogs";
 
+export metadata = {
+  title: "blog | tuna2134",
+  description: "ブログ一覧",
+};
+
 export default async function Page() {
   return (
     <div>
