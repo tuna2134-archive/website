c588051ee46e987077f672ce449abf3060f0d818
mc_fdc <Masato.04.11.2007@gmail.com>
2023-06-02 10:49:04 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <div className="flex-grow">
           <Header />
